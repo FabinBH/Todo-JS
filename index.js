f0c5911e87d350bc4c7a7239e3d75ba22b516e17
Fabio Ferreira <fabiodepaulo2008@gmail.com>
2024-09-11 13:45:55 -0300

    adição do pacote inquirer

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
-let meta = {
-    valor: "Ler um livro",
-    check: false
+const start = () => {
+    while (true) {
+        let opcao = "cadastro"
+        switch (opcao) {
+            case "cadastro":
+                console.log("Vamos cadastrar")
+                break;
+            case "listar":
+                console.log("Vamos listar")
+                break;
+            case "sair":
+                break;
+        }
+    }
 }
 
-const criarMeta = () => {}
+start()
