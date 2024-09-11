e90f92f14f4bb9f664436a474fc70791f9afe244
Fabio Ferreira <fabiodepaulo2008@gmail.com>
2024-09-11 14:04:47 -0300

    criando a função de cadastro

--- index.js
+++ index.js
@@ -1,17 +1,58 @@
-const start = () => {
+const { select, input } = require('@inquirer/prompts')
+
+let meta = {
+    value: "Beber água",
+    check: false
+}
+
+let metas = [meta]
+
+const cadastrarMeta = async () => {
+    const meta = await input({ message: "Digite sua meta: " })
+
+    if (meta.length == 0) {
+        console.log("É preciso escrever uma meta!")
+        return
+    }
+
+    metas.push(
+        { value: meta, check: false}
+    )
+}
+
+const start = async () => {
+
     while (true) {
-        let opcao = "cadastro"
+        const opcao = await select({
+            message: 'MENU >',
+            choices: [
+                {
+                    name: "Cadastrar meta",
+                    value: "cadastro"
+                },
+                {
+                    name: "Listar metas",
+                    value: "listar"
+                },
+                {
+                    name: "Sair",
+                    value: "sair"
+                }
+            ]
+        })
+    
         switch (opcao) {
             case "cadastro":
-                console.log("Vamos cadastrar")
+                await cadastrarMeta()
                 break;
             case "listar":
                 console.log("Vamos listar")
                 break;
             case "sair":
-                break;
+                return;
         }
     }
+
 }
 
 start()
